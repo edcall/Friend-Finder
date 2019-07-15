66c78bb73075cf97b71f00583d9a54cfa8a8a97d
Eddie Callejas <eddiecallejas@hotmail.com>
2019-07-15 00:49:24 -0500

    attempt at path and routes

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 var express = require("express");
+var path = require("path");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
